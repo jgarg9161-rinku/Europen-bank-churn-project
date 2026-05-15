fcc15a69f6a0c7ba7f686530f6e2a321829e7574
jgarg9161-rinku <jgarg9161@gmail.com>
2026-05-15 11:58:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import pickle                                                                                                                                                                                                                                                                                                                 
 import numpy as np
-import plotly.express as pximport plotly.graph_objects as go
+import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime
 import random
 
